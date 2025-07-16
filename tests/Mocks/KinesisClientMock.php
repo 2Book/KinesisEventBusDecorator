52aabfa0a48cd0302f34fb61b57d68557508a37e
aaronbullard <aaron.bullard77@gmail.com>
2025-07-16 16:25:32 -0400

    added documentation

--- tests/Mocks/KinesisClientMock.php
+++ tests/Mocks/KinesisClientMock.php
@@ -5,9 +5,22 @@ namespace Tests\Mocks;
 use Aws\Result;
 use Aws\Kinesis\KinesisClient;
 
+/**
+ * Mock implementation of KinesisClient for testing purposes.
+ * 
+ * This class extends the KinesisClient and overrides the putRecord method
+ * because it is implemented in the KinesisClient by a magic method (__call)
+ * which is not suitable for mocking in tests.
+ */
 class KinesisClientMock extends KinesisClient
 {
-    // Implement any necessary mock methods or properties here
+    /**
+     * Mock implementation of putRecord method.
+     *
+     * @param array $args
+     * @return Result
+     * @throws \BadMethodCallException
+     */
     public function putRecord($args = []): Result
     {
         throw new \BadMethodCallException('putRecord is not implemented in KinesisClientMock');
